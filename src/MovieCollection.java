70994ad55a70dd190e675bda7382880e31dbc9cf
BT_1N3_28 <sample@bths.edu>
2025-02-24 10:29:18 -0500

    imported data method into MovieCollection

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class MovieCollection {
     //instance variables
     private ArrayList<Movie> movies;
+    private Scanner scanner;
 
     //constructor
     public MovieCollection() {
@@ -16,24 +17,47 @@ public class MovieCollection {
         /* ----- EXAMPLE 1 ----- */
         // IMPORT DATA FROM A FILE
         try {
-            File file = new File("src\\movies_data.csv");
-            if (!file.exists()) {
-                return;
-            }
-            Scanner fileScanner = new Scanner(file);
-            while (fileScanner.hasNextLine()) {
+            File myFile = new File("src\\movies_data.csv");
+            Scanner fileScanner = new Scanner(myFile);
+            while (fileScanner.hasNext()) {
                 String data = fileScanner.nextLine();
                 String[] splitData = data.split(",");
-                if (splitData.length == 2) {
-                    String name = splitData[0];
-                    double price = Double.parseDouble(splitData[1]);
-                    movies.add(new Movie(name, price));
-                }
+                String title = splitData[0];
+                String[] cast = new String[]{splitData[1]};
+                String director = splitData[2];
+                String overview = splitData[3];
+                int runtime = Integer.parseInt(splitData[4]);
+                double userRating = Double.parseDouble(splitData[5]);
+                Movie s = new Movie(title, cast, director, overview, runtime, userRating);
+                movies.add(s);
             }
-            fileScanner.close();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+    
+    public void menu() {
+        System.out.println("Welcome to the movie collection!");
+        String menuOption = "";
+
+        while (!menuOption.equals("q")) {
+            System.out.println("------------ Main Menu ----------");
+            System.out.println("- search (t)itles");
+            System.out.println("- search (c)ast");
+            System.out.println("- (q)uit");
+            System.out.print("Enter choice: ");
+            menuOption = scanner.nextLine();
+
+            if (menuOption.equals("t")) {
+                searchTitles();
+            } else if (menuOption.equals("c")) {
+                searchCast();
+            } else if (menuOption.equals("q")) {
+                System.out.println("Goodbye!");
+            } else {
+                System.out.println("Invalid choice!");
+            }
         }
 
-
+    }
 }
